84f8f97ab1f46193cbbe4c708345deb3129d796b
Angela <angelajmg74@gmail.com>
2022-03-31 19:03:57 +0200

    addes exercises solution 1

---
+++ js/algorithms3.js
@@ -0,0 +1,37 @@
+// Exercise 1: solution
+// const exercise1 = () => {
+
+// let minLimit = parseInt(prompt("Insert min number: "));
+// let maxLimit = parseInt(prompt("Insert max number: "));
+
+// let requiredNumbers = 20;
+// let randomNumbersList = new Array(requiredNumbers);
+
+// // console.log(randomNumbersList);
+// // Math.floor(Math.random() * (maxLimit - minLimit + 1)) + minLimit;
+
+// const numberGenerator = (maxLimit, minLImit) =>
+// Math.floor(Math.random() * (maxLimit - minLimit + 1)) + minLimit;
+
+// let minFound = maxLimit;
+// let maxFound = minLimit;
+// for (let i = 0, temp = 0; i<requiredNumbers; i++) {
+//     temp = numberGenerator(maxLimit, minLimit);
+//     randomNumbersList[i] = temp;
+//     if (temp < minFound) {
+//         minFound = temp;
+//     }
+//     if (temp > maxFound) {
+//         maxFound = temp;
+//     }
+// }
+// console.log(randomNumbersList);
+// console.log("Min value: ", minFound);
+// console.log("Max value: ", maxFound);
+
+// }
+
+// EXERCISE 2: SOLUTION
+let date = new new Date();
+alert(date.getHours());
+console.log(now);
